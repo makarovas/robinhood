376aa04915389c37bfb8ac66a91000ab4b322aba
makarovas <makarofalex@gmail.com>
2022-03-29 19:23:25 +0300

    added mocked chart values markup

--- src/componets/Chart.js
+++ src/componets/Chart.js
@@ -1,7 +1,26 @@
 import React from "react";
-
-const Chart = () => {
-  return <div>Chart</div>;
+import "./Chart.css";
+const Chart = ({
+  value = "20000,2",
+  changesTotal = "15000.2",
+  changesPercents = "0.05",
+  dayType = "today",
+  sign = "-",
+}) => {
+  return (
+    <div className="chart">
+      <div className="portfolio">
+        <div className="portfolio__value">${value}</div>
+        <div className="changes">
+          <div className="changes__total">${changesTotal}</div>
+          <div className="changes__percents">
+            {`${sign} ${changesPercents}%`}
+          </div>
+          <div className="dayType">{dayType}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chart;
--- src/componets/Stocks.js
+++ src/componets/Stocks.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Stocks = () => {
-  return <div>Stocks</div>;
+  return <div className="stocks">Stocks</div>;
 };
 
 export default Stocks;
