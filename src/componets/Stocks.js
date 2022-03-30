b8fc9f838a0479bb3714f2737cd6090569617d97
makarovas <makarofalex@gmail.com>
2022-03-30 21:14:40 +0300

    adde get fetch from finnhub

--- src/componets/Stocks.js
+++ src/componets/Stocks.js
@@ -1,7 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import "./Stocks.css";
 
+const BASEURL =
+  "https://finnhub.io/api/v1/quote/add?token=c929l72ad3ibmc67bn1g";
+const JSON = { event: "earnings", symbol: "AAPL" };
 const Stocks = () => {
+  const [stockData, setStockData] = useState({});
+  const [error, setError] = useState({});
+  useEffect(() => {
+    const call = async () => {
+      const responce = await axios.get(BASEURL, JSON).catch((error) => {
+        setError(error);
+      });
+      setStockData(responce);
+    };
+    return call();
+  }, []);
+
+  console.log(stockData);
+  console.log(error);
+
   return (
     <div className="stats">
       <div className="stats__container">
