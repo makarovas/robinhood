90228a082f47930b9095c43185a63c8b729fa5b9
makarovas <makarofalex@gmail.com>
2022-03-30 20:58:28 +0300

    fix graph version

--- src/componets/Stocks.js
+++ src/componets/Stocks.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Stocks.css";
 
 const Stocks = () => {
-  return <div className="stocks">Stocks</div>;
+  return (
+    <div className="stats">
+      <div className="stats__container">
+        <div className="stats__header">
+          <p>Stocks</p>
+        </div>
+        <div className="stats_content">
+          <div className="stats__rows"></div>
+        </div>
+        <div className="stats_header">
+          <p>Lists</p>
+        </div>
+        <div className="stats_content">
+          <div className="stats__rows"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stocks;
